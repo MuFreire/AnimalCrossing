e0516ebda25b5b36a44553e2eec655074b7f82ca
Munique Freire <162357655+MuFreire@users.noreply.github.com>
2024-09-10 01:30:48 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo : "Isabelle",
+        descricao : "Uma secretária e assistente do jogador. Irmã gêmea de Digby.",
+        link : "https://en.wikipedia.org/wiki/Isabelle_(Animal_Crossing)",
+        tags : "irmã secretária secretaria irma assistente"
+    },
+    {
+        titulo : "Tom Nook",
+        descricao : "Um empresário que administra a loja da cidade e o empréstimo imobiliário do jogador.",
+        link : "https://en.wikipedia.org/wiki/Tom_Nook",
+        tags : "empresário loja empréstimo empresario emprestimo"
+    },
+    {
+        titulo : "K.K. Slider",
+        descricao : "Um músico e DJ viajante.",
+        link : "https://en.wikipedia.org/wiki/K.K._Slider",
+        tags : "músico música musico musica dj"
+    },
+];
